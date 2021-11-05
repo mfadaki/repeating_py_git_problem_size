12f75e005a966fc6a376a6a91077d2067e0f4d2e
mfadaki <79007659+mfadaki@users.noreply.github.com>
2021-11-05 13:55:32 +1100

    Add files via upload

---
+++ model_caller.py
@@ -0,0 +1,143 @@
+# region Inputs and Libraries
+from functions.aug_nij import *
+from functions.model_singleP import *
+from functions.model import *
+import os
+import glob
+from functions.create_nij import *
+from inputs.inputs import *
+
+# endregion
+
+# region Planning Horizon
+
+from functions.planning import *
+
+daily_period, weekly_period, remainder_days, phl, ph = planning(60, 14)
+# the second arg is comp time like 14
+
+input_planning = {
+    "daily_period": daily_period,
+    "weekly_period": weekly_period,
+    "remainder_days": remainder_days,
+    "phl": phl,
+    "ph": ph,
+}
+
+with open("./inputs/input_planning.json", "w") as write_file:
+    json.dump(input_planning, write_file, cls=NumpyArrayEncoder)
+
+# endregion
+
+# region Create n_ij
+# Create the n_ij for each sub_multi-period and save it in a json file.
+#
+# - If weekly_period >= daily_period, from the start, we have a full daily period and at least one weekly period. For example, daily_period=5, days=100 --> weekly period = 19. So, we consider the multiperiods of 5*2=10 for a number of times until the period that there will be less than 5 weeks left to the end of horizon. So, for these terminating stages, the length of the multi-period reduces from 10, to 9, then 8, then 7 ....
+# - On the other hand, if weekly_period < daily_period, from the start, we need to start with n_ij with dimension of less than t=2*daily_period.
+# - We need to continue considering the 5d + 5w until, period 95. after that, size of n-ij is reduced by 1 for each iteration as well as the number of weeks to be averaged. 'cc' is avtivated when we get to the critical point of period 95 in this example.
+
+
+create_nij(daily_period, weekly_period, remainder_days, phl, ph)
+# endregion
+
+# region Most recent Json
+# Selct the most recent JSON file in the 'results' directory
+
+list_of_files = glob.glob("./results/n_ij/*.json")
+latest_json_file = max(list_of_files, key=os.path.getctime)
+print(latest_json_file)
+
+
+print(list_of_files)
+
+print(list_of_files[0])
+
+latest_json_file_filename_with_extention = os.path.basename(latest_json_file)
+latest_json_file_filename_without_extention = os.path.splitext(
+    latest_json_file_filename_with_extention
+)[0]
+no_n_ij = int(latest_json_file_filename_without_extention)
+no_n_ij
+# endregion
+
+# region Loop
+
+for c_main in range(no_n_ij + 1):
+    # for c_main in range(3): #c_main is number of cycles
+    with open("./results/n_ij/" + str(c_main) + ".json", "r") as read_file:
+        n_ij_doc = json.load(read_file)
+    n_ij = np.asarray(n_ij_doc["n_ij"])
+    t = len(n_ij[0][0])
+    print(t)
+
+    if c_main != 0:
+        for ci in range(i):
+            for cj in range(j):
+                n_ij[ci, cj, 0] = n_ij[ci, cj, 0] + last_unmet_demand[ci, cj]
+
+    # exec(open("model.py").read())
+    # sol_to_json(c_main, 1)
+    model_fn(1, n_ij, c_main, t)
+
+    with open("./results/solutions/" + str(c_main) + ".json", "r") as read_file:
+        result_last_day = json.load(read_file)
+    last_unmet_demand = np.asarray(result_last_day["unmet_demand"])
+
+
+print(last_unmet_demand)
+
+# endregion
+
+# region (After Target Horizon) Single Period
+# ### Periods after the target horizon (e.g. 60)
+
+# gap_stop = 0.0001
+# c_main = 8
+
+with open("./results/solutions/" + str(c_main) + ".json", "r") as read_file:
+    result_last_day = json.load(read_file)
+last_unmet_demand = np.asarray(result_last_day["unmet_demand"])
+
+
+compt_power = daily_period * 2
+while np.sum(last_unmet_demand) > 0.1:
+    c_main += 1
+    sum_ud = np.sum(last_unmet_demand)
+    avl_stock = k * s * compt_power
+
+    if sum_ud > avl_stock:
+        t_next = compt_power
+    else:
+        t_next_temp = np.sum(last_unmet_demand) / (k * s)
+        if t_next_temp < 1 and t_next_temp > 0:
+            t_next = 1
+        else:
+            t_next = int(t_next_temp) + 1
+
+    n_ij = np.zeros((i, j, t_next))
+    for ct in range(t_next):
+        for ci in range(i):
+            for cj in range(j):
+                n_ij[ci, cj, ct] = int(last_unmet_demand[ci, cj] / t_next)
+
+    nij_json = {"n_ij": n_ij}
+    with open("./results/n_ij/" + str(c_main) + ".json", "w") as write_file:
+        json.dump(nij_json, write_file, cls=NumpyArrayEncoder)
+
+    t = len(n_ij[0][0])
+
+    if t != 1:
+        # exec(open("model.py").read())
+        # sol_to_json(c_main, 0)
+        model_fn(0, n_ij, c_main, t)
+    else:
+        n_ij = n_ij[:, :, 0]
+        # exec(open("model_singleP.py").read())
+        n_ij = aug_nij_fn(n_ij)
+        model_singleP_fn(n_ij, c_main, t)
+
+    with open("./results/solutions/" + str(c_main) + ".json", "r") as read_file:
+        result_last_day = json.load(read_file)
+    last_unmet_demand = np.asarray(result_last_day["unmet_demand"])
+
+# endregion
---
+++ rpt_from_json.py
@@ -0,0 +1,138 @@
+# Instructions:
+# Change the path
+# If number of solutions in the first function is not right, set the number of json solutions manually in line after 40
+# change the parameter name in the last function. for n-ij, we need to select nd_ijt
+
+
+# import numpy as np
+# import scipy as cp
+# import pandas as pd
+# import math
+# import sys
+# import os
+# import json
+# from json import JSONEncoder
+
+path = "./results/results_t6/solutions/"
+
+from functions.import_planning import *
+
+# Selct the most recent JSON file in the 'results' directory
+def no_sol_fn():
+    import glob
+    import os
+
+    list_of_files = glob.glob(path + "*.json")
+    latest_json_file = max(list_of_files, key=os.path.getctime)
+    print(latest_json_file)
+
+    # No of Jsons
+    latest_json_file_filename_with_extention = os.path.basename(latest_json_file)
+    latest_json_file_filename_without_extention = os.path.splitext(
+        latest_json_file_filename_with_extention
+    )[0]
+    no_solions = int(latest_json_file_filename_without_extention)
+    return no_solions
+
+
+no_solions = no_sol_fn()
+print(no_solions)
+
+no_solions = 25  # Use it if we want to manually set the no of solutions
+
+# Get total time ...
+def t_tot_fn(no_solions):
+    t_total = 0
+    with open(path + "0.json", "r") as read_file:
+        sol_json_0 = json.load(read_file)
+    daily_period = sol_json_0["daily_period"]
+    for _ in range(no_solions):
+        with open(path + str(_) + ".json", "r") as read_file:
+            sol_json = json.load(read_file)
+        print(_)
+        print("is_within_target_period= ", sol_json["is_within_target_period"])
+        print("single_period= ", sol_json["single_period"])
+        print("t= ", sol_json["t"])
+
+        if sol_json["t"] >= daily_period:
+            t = daily_period
+        else:
+            t = sol_json["t"]
+        t_total = t_total + t
+    return t_total
+
+
+t_total = t_tot_fn(no_solions)
+print("t_total= ", t_total)
+
+
+def risk_fn(t_total):
+    risk_multi = np.zeros((t_total)).astype(int)
+    c_period = 0
+    for c in range(no_solions + 1):
+        with open(path + str(c) + ".json", "r") as read_file:
+            sol_json = json.load(read_file)
+
+        t = sol_json["t"]
+
+        if t >= daily_period:
+            risk_multi[c_period : c_period + daily_period] = np.asarray(
+                sol_json["risk"]
+            )[:daily_period]
+            c_period += daily_period
+        else:
+            if t == 1:
+                risk_multi[c_period : c_period + t] = np.asarray(sol_json["risk"])
+            else:
+                risk_multi[c_period : c_period + t] = np.asarray(sol_json["risk"])[:t]
+            c_period += t
+    return risk_multi
+
+
+risk_multi = risk_fn(t_total)
+
+np.savetxt("risk_multi.csv", risk_multi, delimiter=",")
+
+# region Any Param Summation
+
+
+def param_sum_fn(t_total, param):
+    param_multi = np.zeros((t_total)).astype(int)
+    c_period = 0
+    for _ in range(no_solions):
+        with open(path + str(_) + ".json", "r") as read_file:
+            sol_json = json.load(read_file)
+
+        t = sol_json["t"]
+        print(t)
+        # print(np.sum(np.asarray(sol_json["n_ij"])))
+        # print(np.sum(np.asarray(sol_json['nd_ij_sv'])))
+        # print(np.sum(np.asarray(sol_json["x_ij_sv"])[:, :, :1]))
+        # print("***")
+
+        if t >= daily_period:
+            for __ in range(daily_period):
+                param_multi[c_period] = np.sum(np.asarray(sol_json[param])[:, :, __])
+                c_period += 1
+        else:
+            if t == 1:
+                if param == "nd_ij_sv":
+                    param = "n_ij"
+                param_multi[c_period] = np.sum(np.asarray(sol_json[param]))
+                c_period += 1
+            else:
+                for __ in range(t):
+                    param_multi[c_period] = np.sum(
+                        np.asarray(sol_json[param])[:, :, __]
+                    )
+                    c_period += 1
+    return param_multi
+
+
+param_multi = param_sum_fn(t_total, "nd_ij_sv")
+
+np.savetxt("param_multi.csv", param_multi, delimiter=",")
+
+# endregion
+
+######################################################
